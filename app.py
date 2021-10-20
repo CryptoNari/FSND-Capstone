3408b0c84567b6656403d4c7e610704d233f7d41
Andreas Vieten <cryptonari@outlook.com>
2021-10-20 14:59:30 +0200

    Error Handler

--- app.py
+++ app.py
@@ -37,6 +37,71 @@ def create_app(test_config=None):
             'message': 'Healthy'
         })
 
+    '''
+    Error handlers for expected errors
+    '''
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "unauthorized"
+        }), 401
+
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return jsonify({
+            "success": False,
+            "error": 403,
+            "message": "forbidden"
+        }), 403
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 500
+
+
+    @app.errorhandler(AuthError)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": error.status_code,
+            "message": error.description
+        }), error.status_code
+
     return app
 
 app = create_app()
