5459e40cf34b28e253259ac39cf6c35786305ccb
Andreas Vieten <cryptonari@outlook.com>
2021-10-20 14:54:14 +0200

    Set up CORS

--- app.py
+++ app.py
@@ -5,20 +5,39 @@ from flask_cors import CORS
 from models import setup_db
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+  
+    '''
+    Set up CORS
+    '''
+    cors = CORS(app, resources={r"/*": {'origins': '*'}})
 
-  @app.route('/', methods={'GET'})
-  def home():
-    
-    return jsonify({
-        'success': True,
-        'message': 'Healthy'
-    })
+    '''
+    set Access-Control-Allow
+    '''
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization'
+        )
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, DELETE, OPTIONS'
+        )
+        return response
 
-  return app
+
+    @app.route('/', methods={'GET'})
+    def home():
+        return jsonify({
+            'success': True,
+            'message': 'Healthy'
+        })
+
+    return app
 
 app = create_app()
 
