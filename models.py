5d424a40f283c1631257ef2e14de4fe3a4e197de
Andreas Vieten <cryptonari@outlook.com>
2021-10-22 15:06:14 +0200

    setup test_app.py 2

--- models.py
+++ models.py
@@ -28,6 +28,13 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
 
+def test_setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
 '''
 Table: Podcasts
 List of all Podcasts
--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 export FLASK_APP=app.py
 export DATABASE_URL='postgresql://postgres:postgres@localhost:5432/capstone'
+
+export TEST_DATABASE_URL='postgresql://postgres:postgres@localhost:5432/capstone_test'
